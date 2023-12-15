3f8e6ff996005c841dc022fa0811fa70f4766dbb
alumne <joanchorto1@iesebre.com>
2023-12-15 16:50:50 +0100

    canvi total de la web

--- src/components/articles/desarrolloWeb2.js
+++ src/components/articles/desarrolloWeb2.js
@@ -3,7 +3,7 @@ import React from "react";
 import Layout from "../layout/layout";
 import {Helmet} from "react-helmet";
 import delta from "../img/delta.jpg"
-import tarragona from "../img/tarragona.jpg";
+
 
 const DesarrolloWeb2 = () => {
 
